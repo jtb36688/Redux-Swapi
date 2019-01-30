4ff417ef6198a19ca12289e4fcddb988bd99e5da
Jacob Bryan <jtb36688@gmail.com>
2019-01-30 16:58:25 -0500

    Compiled Success
    Moved boilerplating to view instead of App. Compiled successfully but not getting anything in the view

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from "react";
-import { getChars } from "./actions/"
-
 import { CharacterListView } from "./views";
 import "./styles/App.css";
-import { connect } from "net";
 
 class App extends Component {
-  componentDidMount() {
 
-
-  }
 
   render() {
     return <CharacterListView />;
   }
 }
 
-const mapStateToProps = state => ({
-  characters: state.characters.characters,
-  error: state.error,
-  isLoading: state.isLoading
-});
 
-export default connect(
-  mapStateToProps,
-  { getChars }(App);
+
+export default App
